49807e93c6a703447769eabd87bd61fe71550791
Stephen Haberman <stephen@bizo.com>
2011-02-18 19:06:47 +0000

    More stubbing.
    git-svn-id: https://bizodev.jira.com/svn/UTIL/trunk/concurrent-test-utils@14302 e6edf6fb-f266-4316-afb4-e53d95876a76

--- src/main/java/com/bizo/concurrent/StubScheduledExecutor.java
+++ src/main/java/com/bizo/concurrent/StubScheduledExecutor.java
@@ -1,10 +1,14 @@
 package com.bizo.concurrent;
 
+import static com.google.common.collect.Lists.newArrayList;
+
+import java.util.List;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
 public class StubScheduledExecutor extends EmptyScheduledExecutorService {
 
+  private final List<FixedRateScheduleFuture> fixedRates = newArrayList();
   private int count = 0;
   private long lastDelay = 0;
   private Runnable command = null;
@@ -13,12 +17,21 @@ public class StubScheduledExecutor extends EmptyScheduledExecutorService {
   @Override
   public ScheduledFuture<?> schedule(final Runnable command, final long delay, final TimeUnit unit) {
     System.out.format("%s %s %s\n", command, delay, unit);
-
     count++;
     lastDelay = delay;
     this.command = command;
-
     return null;
+  }
+
+  @Override
+  public ScheduledFuture<?> scheduleAtFixedRate(
+      final Runnable command,
+      final long initialDelay,
+      final long period,
+      final TimeUnit unit) {
+    final FixedRateScheduleFuture f = new FixedRateScheduleFuture(command, initialDelay, period, unit);
+    fixedRates.add(f);
+    return f;
   }
 
   @Override
@@ -40,6 +53,28 @@ public class StubScheduledExecutor extends EmptyScheduledExecutorService {
 
   public int getShutdown() {
     return shutdown;
+  }
+
+  public List<FixedRateScheduleFuture> getFixedRateScheduleFutures() {
+    return fixedRates;
+  }
+
+  public static class FixedRateScheduleFuture extends StubScheduledFuture<Object> {
+    public final Runnable command;
+    public final long initialDelay;
+    public final long period;
+    public final TimeUnit unit;
+
+    public FixedRateScheduleFuture(
+        final Runnable command,
+        final long initialDelay,
+        final long period,
+        final TimeUnit unit) {
+      this.command = command;
+      this.initialDelay = initialDelay;
+      this.period = period;
+      this.unit = unit;
+    }
   }
 
 }
---
+++ src/main/java/com/bizo/concurrent/StubScheduledFuture.java
@@ -0,0 +1,70 @@
+package com.bizo.concurrent;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Delayed;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class StubScheduledFuture<T> implements ScheduledFuture<T> {
+
+  private final CountDownLatch latch = new CountDownLatch(1);
+  private final AtomicBoolean canceled = new AtomicBoolean(false);
+  private final AtomicBoolean done = new AtomicBoolean(false);
+  private final AtomicReference<T> result = new AtomicReference<T>(null);
+
+  public void done(final T result) {
+    if (done.compareAndSet(false, true)) {
+      this.result.set(result);
+      latch.countDown();
+    }
+  }
+
+  @Override
+  public long getDelay(final TimeUnit unit) {
+    return 0;
+  }
+
+  @Override
+  public int compareTo(final Delayed o) {
+    return 0;
+  }
+
+  @Override
+  public boolean cancel(final boolean mayInterruptIfRunning) {
+    if (canceled.compareAndSet(false, true)) {
+      latch.countDown();
+      return true;
+    }
+    return false;
+  }
+
+  @Override
+  public boolean isCancelled() {
+    return canceled.get();
+  }
+
+  @Override
+  public boolean isDone() {
+    return done.get();
+  }
+
+  @Override
+  public T get() throws InterruptedException, ExecutionException {
+    latch.await();
+    return result.get();
+  }
+
+  @Override
+  public T get(final long timeout, final TimeUnit unit)
+      throws InterruptedException,
+      ExecutionException,
+      TimeoutException {
+    latch.await(timeout, unit);
+    return get();
+  }
+
+}
